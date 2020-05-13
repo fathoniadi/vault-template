e7577e6e625aedfc1c645b1072209eb939e7c245
minhdanh <minhdanh@users.noreply.github.com>
2020-05-13 16:14:37 +0700

    Update package name

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/Luzifer/rconfig"
-	"github.com/actano/vault-template/pkg/template"
+	"github.com/minhdanh/vault-template/pkg/template"
 	"io/ioutil"
 	"log"
 	"os"
